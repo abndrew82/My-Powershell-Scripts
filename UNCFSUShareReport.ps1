35534e82f05608695a5c4a15f2651af0cc58f43c
abndrew82 <apsenn@gmail.com>
2016-11-11 16:51:15 -0500

    Add files via upload
    Added another option and tested

--- UNCFSUShareReport.ps1
+++ UNCFSUShareReport.ps1
@@ -4,7 +4,9 @@
        [parameter(Mandatory=$True,HelpMessage='Enter file age in # of days.')]
        [string]$Days = 'No',
        [parameter(Mandatory=$True,HelpMessage='Location to move files to.')]
-       [string]$Destination = ''
+       [string]$Destination = '',
+       [parameter(Mandatory=$True,HelpMessage='Move files after generating Report (Yes/No)')]
+       [string]$Move = ''
 )
 $ReportName = $share -creplace '(?s)^.*\\', ''
 $Files = Get-ChildItem -Path $Share -Recurse | Where-Object {$_.LastAccessTime -lt (Get-Date).AddDays(-$Days)}
@@ -13,4 +15,7 @@ ForEach ($File in $Files)
 $fileandsize = $file.FullName + "|" + $file.Length
 Out-File $Destination\$ReportName.txt -InputObject $fileandsize -Append
 }
-#Get-ChildItem -Path $Share -Recurse | Where-Object {$_.LastAccessTime -lt (Get-Date).AddDays(-$Days)} | Move-Item -Destination $Destination
+If ($Move -eq "Yes")
+{
+Get-ChildItem -Path $Share -Recurse | Where-Object {$_.LastAccessTime -lt (Get-Date).AddDays(-$Days)} | Move-Item -Destination $Destination
+}
