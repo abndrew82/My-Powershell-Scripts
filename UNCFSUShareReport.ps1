b149874ddc5da118f4f9bc3f4c54001e533708bb
abndrew82 <apsenn@gmail.com>
2016-11-11 07:28:41 -0500

    Add files via upload
    Added share report, gets info from user.  Checks for files by age specified and creates a report of it, and can move the items.

---
+++ UNCFSUShareReport.ps1
@@ -0,0 +1,16 @@
+﻿Param (
+       [parameter(Mandatory=$True,HelpMessage='Enter Share to get file list from.')]
+       [string]$share = '',
+       [parameter(Mandatory=$True,HelpMessage='Enter file age in # of days.')]
+       [string]$Days = 'No',
+       [parameter(Mandatory=$True,HelpMessage='Location to move files to.')]
+       [string]$Destination = ''
+)
+$ReportName = $share -creplace '(?s)^.*\\', ''
+$Files = Get-ChildItem -Path $Share -Recurse | Where-Object {$_.LastAccessTime -lt (Get-Date).AddDays(-$Days)}
+ForEach ($File in $Files)
+{
+$fileandsize = $file.FullName + "|" + $file.Length
+Out-File $Destination\$ReportName.txt -InputObject $fileandsize -Append
+}
+#Get-ChildItem -Path $Share -Recurse | Where-Object {$_.LastAccessTime -lt (Get-Date).AddDays(-$Days)} | Move-Item -Destination $Destination
