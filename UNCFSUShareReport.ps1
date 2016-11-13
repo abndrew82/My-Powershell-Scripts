a55ce2570165c8a84e5b5a8db93156adcafb80c1
abndrew82 <apsenn@gmail.com>
2016-11-13 13:04:48 -0500

    Add files via upload
    Updated script with MB math, File Last access in report.  And Catch if report name is null

--- UNCFSUShareReport.ps1
+++ UNCFSUShareReport.ps1
@@ -1,6 +1,6 @@
 ﻿Param (
        [parameter(Mandatory=$True,HelpMessage='Enter Share to get file list from.')]
-       [string]$share = '',
+       [string]$Share = '',
        [parameter(Mandatory=$True,HelpMessage='Enter file age in # of days.')]
        [string]$Days = 'No',
        [parameter(Mandatory=$True,HelpMessage='Location to move files to.')]
@@ -8,11 +8,16 @@
        [parameter(Mandatory=$True,HelpMessage='Move files after generating Report (Yes/No)')]
        [string]$Move = ''
 )
-$ReportName = $share -creplace '(?s)^.*\\', ''
+$ReportName = $Share -creplace '(?s)^.*\\', ''
 $Files = Get-ChildItem -Path $Share -Recurse | Where-Object {$_.LastAccessTime -lt (Get-Date).AddDays(-$Days)}
 ForEach ($File in $Files)
 {
-$fileandsize = $file.FullName + "|" + $file.Length
+If ($ReportName -eq "$null")
+{
+$ReportName = "UNCFSUShareDefault"
+}
+$filesize = $file.Length/1MB
+$fileandsize = $file.FullName + "|" + $file.LastAccessTime + "|" + $filesize
 Out-File $Destination\$ReportName.txt -InputObject $fileandsize -Append
 }
 If ($Move -eq "Yes")
